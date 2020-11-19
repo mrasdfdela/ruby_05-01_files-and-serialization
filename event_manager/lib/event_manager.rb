727e00cf955db1eb4f6d496d6af493dc9b79e376
mrasdfdela <jerryhsu830@gmail.com>
2020-11-18 23:52:41 -0500

    Iteration 2.1 - Clearing Up Our Zip Codes

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -3,11 +3,21 @@ require "csv"
 
 puts "EventManager Initialized!"
 
+def zip_code_converter(code)
+  # code = "" if code.nil?
+  # code = code[0..4] if code.length > 5
+  # until code.length == 5
+  #   code = "0" + code
+  # end
+  # return code
+  code.to_s.rjust(5,"0")[0..4]
+end
+
 if File.exist? "event_attendees.csv"
   contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
   contents.each do |row|
     name = row[:first_name]
-    zipcode = row[:zipcode]
+    zipcode = zip_code_converter(row[:zipcode])
 
     puts "#{name}, #{zipcode}"
   end

05a5e7775ae6b8d7f64c9f76fc83f3c5377f49d3
mrasdfdela <jerryhsu830@gmail.com>
2020-11-22 17:52:26 -0500

    Iteration 4: Form Letters; Event Manager: hour count, dow count

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,6 +1,9 @@
 require "byebug"
 require "csv"
 
+require "date"
+require "time"
+
 puts "EventManager Initialized!"
 
 def zip_code_converter(code)
@@ -13,12 +16,43 @@ def zip_code_converter(code)
   code.to_s.rjust(5,"0")[0..4] #Coercion over questions
 end
 
+def clean_phone_numbers(phone)
+  phone = phone.to_s.split('')
+  phone.delete_if { |char| false if Float(char) rescue true }
+  phone.shift if phone[0] == 1.to_s
+
+  phone.length == 10 ? format_phone(phone.join('')) : 'Invalid phone number'
+end
+def format_phone(phone)
+  area_cd = "#{phone[0..2]}"
+  exc_cd = "#{phone[3..5]}"
+  subscriber = "#{phone[6..9]}"
+  "(#{area_cd}) #{exc_cd}-#{subscriber}"
+end
+
+def hour_counter(count, date)
+  count[date.hour] ? count[date.hour] += 1 : count[date.hour] = 1
+end
+
+def dow_counter(count, date)
+  count[date.wday] ? count[date.wday] += 1 : count[date.wday] = 1
+end
+
 if File.exist? "event_attendees.csv"
   contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+  hour_count = {}
+  dow_count = {}
   contents.each do |row|
     name = row[:first_name]
     zipcode = zip_code_converter(row[:zipcode])
+    phone_number = clean_phone_numbers(row[:homephone])
+    reg_date = DateTime.strptime(row[:regdate], '%m/%d/%y %k: %M')
 
-    puts "#{name}, #{zipcode}"
+    hour_counter(hour_count, reg_date)
+    dow_counter(dow_count, reg_date)
+
+    puts "#{name}, #{zipcode}, #{phone_number}, #{reg_date.hour}"
   end
+  puts hour_count.sort_by{ |k,v| -v}.to_h
+  puts dow_count.sort_by{ |k,v| -v}.to_h
 end
--- event_manager/lib/google-civic-api.rb
+++ event_manager/lib/google-civic-api.rb
@@ -2,6 +2,7 @@ require 'byebug'
 
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -23,14 +24,30 @@ def legislators_by_zipcode(zip)
   end
 end
 
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists? "output"
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
 puts "EventManager initialized."
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  
-  puts "#{name}, #{zipcode}: #{legislators}"
+  # puts "#{name}, #{zipcode}: #{legislators}"
+  # personal_letter = template_letter.gsub!('FIRST_NAME', name)
+  # personal_letter = template_letter.gsub!('LEGISLATORS', name)
+  # puts personal_letter
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id,form_letter)
 end
+
